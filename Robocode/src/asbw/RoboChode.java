8ace10d8c7cfc85a6940aa47436928c2de85d003
Andrew Spaziani <spaziani96@gmail.com>
2018-09-20 11:34:05 -0400

    BetterBot

--- Robocode/src/asbw/RoboChode.java
+++ Robocode/src/asbw/RoboChode.java
@@ -2,6 +2,7 @@ package asbw;
 
 import java.awt.Color;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 import robocode.*;
 import robocode.util.Utils;
@@ -29,6 +30,8 @@ public class RoboChode extends AdvancedRobot {
     static double oldEnemyHeading;
     static double enemyEnergy;
 
+    public double wallStick = 120;
+
 
     public void run(){
 
@@ -44,27 +47,61 @@ public class RoboChode extends AdvancedRobot {
     }
     public void onScannedRobot(ScannedRobotEvent e){
 
-        // calculate firepower based on distance
-        double firePower = Math.min(350 / e.getDistance(), 3);
-        // calculate speed of bullet
-        double bulletSpeed = 25 - firePower * 3;
+        double bulletSpeed = 25 - firePower * 4;
         // distance = rate * time, solved for time
         long time = (long)(e.getDistance() / bulletSpeed);
 
-        setTurnRight(e.getBearing()+60*movementDirection);
+
+
+
+        setTurnRight(e.getBearing()+75*movementDirection);
         double absBearing=e.getBearingRadians()+getHeadingRadians();
         double absoluteBearing = getHeadingRadians() + e.getBearingRadians();//robot's absolute bearing
-        double randomGuessFactor = (Math.random() - .5) * 4;
-        double maxEscapeAngle = Math.asin(5.0/(10 - (3 *Math.min(2,getEnergy()/10))));//farthest the enemy can move in the amount of time it would take for a bullet to reach them
+        double randomGuessFactor = (Math.random() - .8) * 4;
+        double maxEscapeAngle = Math.asin(6.0/(10 - (4 *Math.min(3,getEnergy()/10))));//farthest the enemy can move in the amount of time it would take for a bullet to reach them
         double randomAngle = randomGuessFactor * maxEscapeAngle;//random firing angle
         double firingAngle = Utils.normalRelativeAngle(absoluteBearing - getGunHeadingRadians()+randomAngle/5);//amount to turn our gun
-        setTurnRightRadians(e.getBearingRadians()*(dir*15));//Turn perpendicular to them
-        setTurnGunRightRadians(firingAngle);//Aim!
-        setAhead(100*Math.random()*(dir*4));
-        setFire(getEnergy()+ 7);//Fire, using less energy if we have low energy
-        setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing-getRadarHeadingRadians()));//lock on the radar
-        if(Math.random() > .875){
-            dir=-dir;
+
+        absBearing=e.getBearingRadians()+getHeadingRadians();//enemies absolute bearing
+        double latVel=e.getVelocity() * Math.sin(e.getHeadingRadians() -absBearing);//enemies later velocity
+        double gunTurnAmt;//amount to turn our gun
+        setTurnRadarLeftRadians(getRadarTurnRemainingRadians());//lock on the radar
+        if(Math.random()>.6){
+            setMaxVelocity((12*Math.random())+12);//randomly change speed
+        }
+        if (e.getDistance() > 250) {
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/15);
+            setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing-getHeadingRadians()+latVel/getVelocity()));//Turn perpendicular to them
+            setTurnGunRightRadians(gunTurnAmt);//Aim!
+            setAhead((e.getDistance() - 140)*movementDirection);
+            setFire(firePower/1.5);//Fire, using less energy if we have low energy
+            setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing - getRadarHeadingRadians()));//lock on the radar
+            if (Math.random() > .750) {
+                dir = -dir;
+            }
+        }
+
+        else if (e.getDistance() > 150) {//if distance is greater than 150
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/18);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt); //turn our gun
+            setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(absBearing-getHeadingRadians()+latVel/getVelocity()));//drive towards the enemies predicted future location
+            setAhead((e.getDistance() - 140)*movementDirection);//move forward
+            setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing - getRadarHeadingRadians()));
+            setFire(firePower);//fire
+            if (Math.random() > .6) {
+                dir = -dir;
+            }
+        }
+        else {//if we are close enough...
+            gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing- getGunHeadingRadians()+latVel/12);//amount to turn our gun, lead just a little bit
+            setTurnGunRightRadians(gunTurnAmt);//turn our gun
+            setTurnLeft(-90-e.getBearing()); //turn perpendicular to the enemy
+            setAhead((e.getDistance() - 140)*movementDirection);//move forward
+            setTurnRadarRightRadians(Utils.normalRelativeAngle(absoluteBearing - getRadarHeadingRadians()));
+            setFire(firePower*1.5);//fire
+            if (Math.random() > .5) {
+                dir = -dir;
+            }
         }
 
         //This makes the amount we want to turn be perpendicular to the enemy.
@@ -91,47 +128,29 @@ public class RoboChode extends AdvancedRobot {
 
         setAhead(270*dir);
 
-        //Finding the heading and heading change.
-        double enemyHeading = e.getHeadingRadians();
-        double enemyHeadingChange = enemyHeading - oldEnemyHeading;
-        oldEnemyHeading = enemyHeading;
-
-		/*This method of targeting is know as circular targeting; you assume your enemy will
-		 *keep moving with the same speed and turn rate that he is using at fire time.The
-		 *base code comes from the wiki.
-		*/
-        double deltaTime = 0;
-        double predictedX = getX()+e.getDistance()*Math.sin(absBearing);
-        double predictedY = getY()+e.getDistance()*Math.cos(absBearing);
-        while((++deltaTime) * bulletSpeed <  Point2D.Double.distance(getX(), getY(), predictedX, predictedY)){
-
-            //Add the movement we think our enemy will make to our enemy's current X and Y
-            predictedX += Math.sin(enemyHeading) * e.getVelocity();
-            predictedY += Math.cos(enemyHeading) * e.getVelocity();
-
-
-            //Find our enemy's heading changes.
-            enemyHeading += enemyHeadingChange;
-
-            //If our predicted coordinates are outside the walls, put them 18 distance units away from the walls as we know
-            //that that is the closest they can get to the wall (Bots are non-rotating 36*36 squares).
-            predictedX=Math.max(Math.min(predictedX,getBattleFieldWidth()-18),18);
-            predictedY=Math.max(Math.min(predictedY,getBattleFieldHeight()-18),18);
-
-        }
-        //Find the bearing of our predicted coordinates from us.
-        double aim = Utils.normalAbsoluteAngle(Math.atan2(  predictedX - getX(), predictedY - getY()));
-
         //Aim and fire.
-        setTurnGunRightRadians(Utils.normalRelativeAngle(aim - getGunHeadingRadians()));
         setFire(firePower);
 
-
-
-
-
-
         setTurnRadarRightRadians(Utils.normalRelativeAngle(absBearing-getRadarHeadingRadians())*2);
+
+        double goalDirection = absBearing-Math.PI/2.0*movementDirection;
+
+		/* This is too clean for crazy! Add some randomness. */
+        goalDirection += (Math.random()-0.5) * (Math.random()*2.0 + 1.0);
+
+		/* Smooth around the walls, if we smooth too much, reverse direction! */
+        double x = getX();
+        double y = getY();
+        double smooth = 0;
+
+		/* Calculate the smoothing we would end up doing if we actually smoothed walls. */
+        Rectangle2D fieldRect = new Rectangle2D.Double(18, 18, getBattleFieldWidth()-36, getBattleFieldHeight()-36);
+
+        while (!fieldRect.contains(x+Math.sin(goalDirection)*wallStick, y+ Math.cos(goalDirection)*wallStick)) {
+			/* turn a little toward enemy and try again */
+            goalDirection += movementDirection*0.1;
+            smooth += 0.1;
+        }
     }
     public void onBulletHit(BulletHitEvent e){
         enemyEnergy-=(BULLET_DAMAGE * 10);
